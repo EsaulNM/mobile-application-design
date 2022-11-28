25b3c020d01ed93d077166d11d49a1dc24c2577f
EsaulNM <esaulnm@outlook.com>
2022-11-28 14:58:31 -0600

    Producto integrador

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Esaul\\Desktop\\my_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\Esaul\\Desktop\\my_app"
+  "FLUTTER_ROOT=D:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Esaul\\Desktop\\my_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Esaul\\Desktop\\my_app"
+  "FLUTTER_TARGET=C:\\Users\\Esaul\\Desktop\\my_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Esaul\\Desktop\\my_app\\.dart_tool\\package_config.json"
+)
